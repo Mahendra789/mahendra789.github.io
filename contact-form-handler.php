b82ee069ee8b4b7ee46c2e2dce80c03bd8c622ee
mahendra <mahendra.ahada@beretail.in>
2021-01-30 20:08:14 +0530

    UI issue fixed, contact from handler added, images changed.

---
+++ contact-form-handler.php
@@ -0,0 +1,36 @@
+<?php
+$errors = '';
+$myemail = 'ahada.mahendra@gmail.com';
+if(empty($_POST['name'])  ||
+   empty($_POST['email']) ||
+   empty($_POST['mobile']) ||
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$mobile = $_POST['mobile'];
+$message = $_POST['message'];
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+$to = $myemail;
+$email_subject = "Contact form submission: $name";
+$email_body = "You have received a new message. ".
+"Here are the details:\n Name: $name \n ".
+"Mobile: $mobile\n"
+"Email: $email_address\n Message \n $message";
+$headers = "From: $myemail\n";
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+//redirect to the 'thank you' page
+header('Location: index.html');
+}
+?>
